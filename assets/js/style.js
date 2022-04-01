e65cd8bf7dc2268a0230bb2922cba45fe7cd53fa
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-04-01 01:06:53 -0600

    Created functioning add task button

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,1 +1,11 @@
-console.dir(window.document);
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoE1 = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function () {
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
+    listItemE1.textContent = "This is a new task";
+    tasksToDoE1.appendChild(listItemE1);
+};
+
+buttonEl.addEventListener("click", createTaskHandler);
