03e4db7186c64bc9ac2c32877740743de9e23cd8
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-04-02 10:43:28 -0600

    added form functionality and prevented default submit action from the browser

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,11 +1,14 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+    event.preventDefault();
+
     var listItemE1 = document.createElement("li");
+
     listItemE1.className = "task-item";
     listItemE1.textContent = "This is a new task";
     tasksToDoE1.appendChild(listItemE1);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
